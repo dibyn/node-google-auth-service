36d1f3a41b43ca73af50760148f8cca3c0043d26
Dibyn <dibyn7@gmail.com>
2020-11-20 13:26:51 +0545

    removed credentials for security reasons

--- server-side/firebase/index.js
+++ server-side/firebase/index.js
@@ -1,5 +1,5 @@
 const firebase = require('firebase-admin')
-const credentials = require('./fir-auth-server-2a5d4-firebase-adminsdk-6g7rk-17fdbdf11c.json')
+const credentials = require('./credentials.json')
 firebase.initializeApp({
   credential: firebase.credential.cert(credentials),
   databaseURL: 'https://fir-auth-server-2a5d4.firebaseio.com',
